628da7b4a0a4866efe6c74d7e97a909e09145f67
Ayodeji Hamed <dhamed007@gmail.com>
2022-09-08 11:28:19 +0000

    donethat

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,4 @@
 # Deploying a python web application
 echo "Hello World" = shell script
 print('Hello World') = python script
-<<<<<<< HEAD
 deploy in dec environment
-Thank you.
-<<<<<<< HEAD
-deployment5 master
-=======
-deployment4
-deployment4
-deployment4
-
->>>>>>> develop
-
